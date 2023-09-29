28dd2285ea106f44364076b2546ca6aeec00246b
bttu-ka <141981341+bttu-ka@users.noreply.github.com>
2023-09-29 23:50:35 +1000

    Add files via upload

---
+++ src/views/AboutView.vue
@@ -0,0 +1,199 @@
+<template>
+  <div class="about-container">
+    <header class="custom-header">
+      <h1 class="custom-title">Akshat's Portfolio</h1>
+    </header>
+    <div class="content-container">
+      <div class="intro-container">
+        <h2 class="custom-subtitle">{{ name }}</h2>
+        <p class="custom-intro">{{ intro }}</p>
+      </div>
+      
+      <div v-for="project in projects" :key="project.title" class="project-container">
+        <h2 class="project-title">{{ project.title }}</h2>
+        <p class="project-description">{{ project.description }}</p>
+        <a :href="project.link" target="_blank" rel="noopener noreferrer" class="custom-link">View</a>
+      </div>
+      <div class="contact-container">
+        <p class="contact-title">My credentials:</p>
+        <p class="contact-info">Email: {{ email }}</p>
+        <p class="contact-info">Student ID: {{ studentid }}</p>
+        <p class="contact-info">Phone: {{ phone }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      intro: 'This is my portfolio webste showcasing my projects and my credentials.',
+      studentid: 'S223307311',
+      
+      projects: [
+        {
+          title: 'Project-1',
+          description: 'For this project, I have created a To-Do list app using HTML, CSS and a variety of Vue3 concepts. ',
+          link: 'https://github.com/bttu-ka/2.1C'
+        },
+        {
+          title: 'Project-2',
+          description: 'For this project, I have created a website called Travelscape,which users can use to plan adn organize their trips efficiently.',
+          link: 'https://github.com/bttu-ka/task2.2c'
+        },
+        {
+          title: 'Project-3',
+          description: 'For this project, I have created a Responsive webpage using HTML and CSS.',
+          link: 'https://github.com/bttu-ka/TASK1.2P-RESPONSIVE-WEB-APPS-'
+        },
+      ],
+      email: 'akshataswa@deakin.us',
+      phone: '0424XXXXXX'
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+body {
+  background-color: white;
+  font-family: 'Arial', sans-serif;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.about-container {
+  width: 100%;
+  max-width: 1200px;
+  padding: 20px;
+  box-sizing: border-box;
+}
+
+
+.custom-header {
+  background-color: #3498db;
+  color: #fff;
+  padding: 20px 0;
+  text-align: center;
+}
+
+.custom-title {
+  font-family: 'arial',sans-serif;
+  margin: 0;
+  font-size: 36px;
+}
+
+
+.content-container {
+  background-color: #fff;
+  border-radius: 10px;
+  padding: 30px;
+  margin: 20px 0;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.2);
+}
+
+.intro-container {
+  text-align: center;
+}
+
+.custom-subtitle {
+  font-family: 'arial',sans-serif;
+  color: #3498db;
+  font-size: 24px;
+}
+
+.custom-intro {
+  color: #555;
+  font-size: 18px;
+  margin-top: 20px;
+}
+
+
+.skills-container {
+  margin-top: 20px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.skill {
+  background-color: pink;
+  color: #fff;
+  padding: 8px 16px;
+  margin: 5px;
+  border-radius: 3px;
+  text-transform: uppercase;
+  font-weight: bold;
+}
+
+
+.project-container {
+  background-color: #ecf0f1;
+  border-radius: 5px;
+  padding: 20px;
+  margin: 20px 0;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  text-align: center;
+}
+
+.project-title {
+  color: #3498db;
+  font-family: 'ariel', sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.project-description {
+  color: #333;
+  margin-top: 10px;
+}
+
+.custom-link {
+  display: inline-block;
+  background-color: #27ae60;
+  color: #fff;
+  padding: 10px 20px;
+  text-decoration: none;
+  border-radius: 5px;
+  transition-duration: 0.4s;
+  font-weight: bold;
+  margin-top: 10px;
+}
+
+.custom-link:hover {
+  background-color: #1e8449;
+}
+
+
+.contact-container {
+  background-color: #ecf0f1;
+  border-radius: 5px;
+  padding: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+  text-align: center;
+  font-weight: bold;
+}
+
+
+.contact-title {
+  color: #3498db;
+  font-size: 24px;
+  font-weight: bold;
+}
+
+.contact-info {
+  color: #333;
+  margin-top: 10px;
+  font-weight: bold;
+}
+
+.contact-info:hover {
+  color: #000;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="contact-form">
+      <h1 style="background-color: #ff5733; color: white;">Contact Form</h1>
+      <form @submit.prevent="submitForm">
+        <label for="name">Name:</label>
+        <input id="name" v-model="formData.name" required placeholder="Your name" style="margin-bottom: 15px;">
+          
+        <label for="email">Email:</label>
+        <input id="email" v-model="formData.email" type="email" required placeholder="Your e-mail" style="margin-bottom: 15px;">
+      
+        <label for="message">Message:</label>
+        <textarea id="message" v-model="formData.message" rows="8" required placeholder="Any message for me? Type it here." style="margin-bottom: 15px;"></textarea>
+      
+        <button type="submit" class="submit-button">Submit</button>
+      </form>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        formData: {
+          name: '',
+          email: '',
+          message: ''
+        }
+      };
+    },
+    methods: {
+      submitForm() {
+        console.log('Form Data:', this.formData);
+        alert('The form is sent, wait for me to reply to it. Thanks.');
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Global Styles */
+  body, h1, h2, p, label, input, textarea, button {
+    margin: 0;
+    padding: 0;
+    font-family: Arial, sans-serif;
+  }
+  
+  /* Container Styles */
+  .contact-form {
+    background-color: yellowgreen; /* Updated background color */
+    padding: 40px; /* Increased padding to make it taller */
+    border-radius: 10px;
+    box-shadow: 0 4px 12px rgba(237, 173, 24, 0.1);
+    max-width: 400px;
+    width: 90%;
+    color: white; /* Text color */
+  }
+  
+  /* Label Styles */
+  label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 10px; /* Increased margin for more spacing */
+    font-family: 'Pacifico', cursive;
+    color: #ff5733; /* Label text color */
+  }
+  
+  /* Input and Textarea Styles */
+  input[type="text"],
+  input[type="email"],
+  textarea {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 15px;
+    border: 1px solid #c9f0d6;
+    border-radius: 5px;
+    transition: border-color 0.3s;
+  }
+  
+  input[type="text"]:focus,
+  input[type="email"]:focus,
+  textarea:focus {
+    border-color: #72b8a0;
+  }
+  
+  /* Textarea Resize Styles */
+  textarea {
+    resize: vertical;
+  }
+  
+  /* Submit Button Styles */
+  .submit-button {
+    background-color: #ff5733; /* Updated button color */
+    color: white;
+    border: none;
+    padding: 12px 20px;
+    border-radius: 5px;
+    cursor: pointer;
+    font-size: 16px;
+    transition: background-color 0.3s;
+  }
+  
+  /* Media Query for Small Screens */
+  @media (max-width: 768px) {
+    .contact-form {
+      width: 100%;
+      padding: 20px;
+    }
+  }
+  </style>
+  
